85de0e975c9759a959b3be312b0156d4e93fd6eb
Alex <cherniav@Alexs-MacBook-Pro-5.local>
2023-01-21 14:28:18 -0800

    added map functionality, can't pull map tiles though

--- client/app/src/main/java/com/cherniav/r2f/MapFragment.java
+++ client/app/src/main/java/com/cherniav/r2f/MapFragment.java
@@ -1,5 +1,6 @@
 package com.cherniav.r2f;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,7 +9,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.osmdroid.api.IMapController;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
 public class MapFragment extends Fragment {
+
+    // Declarations Setup
+    private MapView map;
+    private MyLocationNewOverlay mLocationOverlay;
+
     public MapFragment() {
         // Required empty public constructor
     }
@@ -26,7 +38,35 @@ public class MapFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        final Context context = this.getActivity();
+
+        View mapView = inflater.inflate(R.layout.fragment_map, null);
+
+        map = mapView.findViewById(R.id.map);
+
+        // Set controls
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+
+        // Set default map zoom via Map Controller
+        // ////////(NOTE: make variable later)
+        IMapController mapController = map.getController();
+
+        mapController.setZoom(8);
+
+        // Set start point
+        GeoPoint startPoint = new GeoPoint(49.2827, -123.1207);
+
+        // Set map initialization point
+        mapController.setCenter(startPoint);
+
+        // Create overlay for current location
+        mLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(context),map);
+        mLocationOverlay.enableMyLocation();
+        map.getOverlays().add(this.mLocationOverlay);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        return mapView;
     }
 }
