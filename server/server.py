35be5e017c4e60e233feaa1825c547908606ed1b
emern <enagyjr@nagy-fam.com>
2023-01-21 12:42:48 -0800

    adding test client and server for infra verficiation

--- server/server.py
+++ server/server.py
@@ -1,1 +1,17 @@
-"""This is a server"""
+"""This is a server - basic test"""
+
+from socket import *
+
+PORT = 12000
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind(('', PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
---
+++ server/test_client.py
@@ -0,0 +1,13 @@
+""" Test client """
+from socket import *
+
+HOST = '207.81.215.40'
+PORT = 12000
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b"Hello, world")
+    data = s.recv(1024)
+
+print(f"Received {data!r}")
