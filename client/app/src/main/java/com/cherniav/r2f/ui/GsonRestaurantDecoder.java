b51a1265f2c8d35a023a4c8366e5bf0d094ca5ae
emern <enagyjr@nagy-fam.com>
2023-01-22 08:43:48 -0800

    socket changes 2

--- client/app/src/main/java/com/cherniav/r2f/ui/GsonRestaurantDecoder.java
+++ client/app/src/main/java/com/cherniav/r2f/ui/GsonRestaurantDecoder.java
@@ -1,2 +1,10 @@
-package com.cherniav.r2f.ui;public class GsonRestaurantDecoder {
+package com.cherniav.r2f.ui;
+
+import com.cherniav.r2f.RestaurantInfo;
+
+import java.util.ArrayList;
+
+public class GsonRestaurantDecoder {
+    String response;
+    ArrayList<RestaurantInfo> restaurants;
 }
