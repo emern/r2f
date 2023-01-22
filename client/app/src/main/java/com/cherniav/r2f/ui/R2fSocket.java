64b05aff65eb12818aa86f61bc8773f7324d28d2
emern <enagyjr@nagy-fam.com>
2023-01-22 08:44:41 -0800

    more socket changes

--- client/app/src/main/java/com/cherniav/r2f/ui/R2fSocket.java
+++ client/app/src/main/java/com/cherniav/r2f/ui/R2fSocket.java
@@ -16,8 +16,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class R2fSocket {
-    private static String hostname = "207.81.215.40";
-    private static int port = 12000;
 
     private static String RESP_OK = "OK";
 
@@ -25,7 +23,15 @@ public class R2fSocket {
         JsonObject jsonObject = new JsonObject();
         jsonObject.addProperty("cmd", 0);
         jsonObject.addProperty("name", name);
-        String reponse_data = getResponse(jsonObject.toString());
+        ResponseGetter getter = new ResponseGetter(jsonObject.toString());
+        getter.start();
+        try {
+            getter.join();
+        }
+        catch(InterruptedException e) {
+           return new ArrayList<>();
+        }
+        String reponse_data = getter.response;
         Gson gson = new Gson();
         GsonRestaurantDecoder decoder =  gson.fromJson(reponse_data, GsonRestaurantDecoder.class);
         if (decoder.response.equals(RESP_OK)) {
@@ -42,7 +48,15 @@ public class R2fSocket {
         jsonObject.addProperty("long", longitude);
         jsonObject.addProperty("num", num_search);
         jsonObject.addProperty("radius", radius);
-        String reponse_data = getResponse(jsonObject.toString());
+        ResponseGetter getter = new ResponseGetter(jsonObject.toString());
+        getter.start();
+        try {
+            getter.join();
+        }
+        catch(InterruptedException e) {
+            return new ArrayList<>();
+        }
+        String reponse_data = getter.response;
         Gson gson = new Gson();
         GsonRestaurantDecoder decoder =  gson.fromJson(reponse_data, GsonRestaurantDecoder.class);
         if (decoder.response.equals(RESP_OK)) {
@@ -56,42 +70,19 @@ public class R2fSocket {
         JsonObject jsonObject = new JsonObject();
         jsonObject.addProperty("cmd", 1);
         jsonObject.addProperty("placeid", placeid);
-        String reponse_data = getResponse(jsonObject.toString());
+        ResponseGetter getter = new ResponseGetter(jsonObject.toString());
+        getter.start();
+        try {
+            getter.join();
+        }
+        catch(InterruptedException e) {
+            return false;
+        }
+        String reponse_data = getter.response;
         Gson gson = new Gson();
         GsonRestaurantDecoder decoder =  gson.fromJson(reponse_data, GsonRestaurantDecoder.class);
         return (decoder.response.equals(RESP_OK)) ? true : false;
     }
 
-
-    private static String getResponse(String sendData) {
-        try (Socket socket = new Socket(hostname, port)) {
-
-            OutputStream output = socket.getOutputStream();
-            PrintWriter writer = new PrintWriter(output, true);
-            writer.print(sendData);
-            writer.flush();
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            String outLine = "";
-            for (String line = reader.readLine(); (line != null); line = reader.readLine()) {
-                outLine = line;
-            }
-            output.close();
-            reader.close();
-            return outLine;
-
-        } catch (UnknownHostException ex) {
-            System.out.println("Server not found: " + ex.getMessage());
-        } catch (IOException ex) {
-            System.out.println("I/O error: " + ex.getMessage());
-        }
-
-        return "";
-    }
-
-    private class GsonRestaurantDecoder {
-        String response;
-        ArrayList<RestaurantInfo> restaurants;
-    }
 
 }
